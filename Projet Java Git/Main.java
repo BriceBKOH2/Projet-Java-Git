4c907f8b82299f667bbeeefd75549723c36bd7e3
BriceB <brice.beteille@gmail.com>
2019-10-14 10:50:10 +0200

    New branch Test + modified last print on Main + added Personnage.java

--- Projet Java Git/Main.java
+++ Projet Java Git/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-    system.out.println("Hello ! New project java here to test Git functions")
+    system.out.println("Hello ! New project java here to test Git functions");
 
+    system.out.println("A bientot");
   }
